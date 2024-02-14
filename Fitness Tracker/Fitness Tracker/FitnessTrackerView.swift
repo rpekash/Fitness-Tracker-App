7bf138ab2c6deb3d6eab6fb3731cb4de603268bb
Kastrijot Syla <ksyla@SCE-C02WQ0R9HV2T.quinnipiac.edu>
2024-02-14 17:52:27 -0500

    initial commit

---
+++ Fitness Tracker/Fitness Tracker/FitnessTrackerView.swift
@@ -0,0 +1,28 @@
+//
+//  FitnessTrackerView.swift
+//  Fitness Tracker
+//
+//  Created by Kastrijot Syla on 2/14/24.
+//
+
+import SwiftUI
+
+struct FitnessTrackerView: View 
+{
+    var body: some View 
+    {
+        VStack 
+        {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview 
+{
+    FitnessTrackerView()
+}
---
+++ Fitness Tracker/Fitness Tracker/Fitness_TrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Fitness_TrackerApp.swift
+//  Fitness Tracker
+//
+//  Created by Kastrijot Syla on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_TrackerApp: App 
+{
+    var body: some Scene 
+    {
+        WindowGroup 
+        {
+            FitnessTrackerView()
+        }
+    }
+}
