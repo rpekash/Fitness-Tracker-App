9cc8edbc96df78b2a1dc72bb19b38e1886aeeb9b
Kastrijot Syla <ksyla@SCE-C02WQ0R9HV2T.quinnipiac.edu>
2024-02-14 18:10:44 -0500

    initial commit 2.5

--- Fitness Tracker/Fitness Tracker/Resources/Fitness_TrackerApp.swift
+++ Fitness Tracker/Fitness Tracker/Resources/Fitness_TrackerApp.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 @main
-struct Fitness_TrackerApp: App {
-    var body: some Scene {
+struct Fitness_TrackerApp: App 
+{
+    var body: some Scene 
+    {
         WindowGroup {
-            ContentView()
+            FitnessTrackerView()
         }
     }
 }
