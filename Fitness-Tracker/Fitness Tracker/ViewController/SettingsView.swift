f170a082c9a858562c3ad4a9b8f4c7c0aae58cf3
Kastrijot Syla <ksyla@SCE-C02WQ0R9HV2T.quinnipiac.edu>
2024-04-15 11:14:35 -0400

    Settings Tab Added

--- Fitness-Tracker/Fitness Tracker/ViewController/SettingsView.swift
+++ Fitness-Tracker/Fitness Tracker/ViewController/SettingsView.swift
@@ -7,14 +7,35 @@
 
 import SwiftUI
 
-struct SettingsView: View 
-{
-    var body: some View 
-    {
-        Text("Settings")
+struct SettingsView: View {
+    @AppStorage("notificationsEnabled") var notificationsEnabled: Bool = true
+    @AppStorage("darkModeEnabled") var darkModeEnabled: Bool = false
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("General")) {
+                    Toggle("Enable Notifications", isOn: $notificationsEnabled)
+                    Toggle("Dark Mode", isOn: $darkModeEnabled)
+                    Button("Clear All Data", role: .destructive) {
+                        clearAllData()
+                    }
+                }
+            }
+            .navigationTitle("Settings")
+        }
+    }
+
+    // Function to clear all data
+    private func clearAllData() {
+        UserDefaults.standard.removeObject(forKey: "notificationsEnabled")
+        UserDefaults.standard.removeObject(forKey: "darkModeEnabled")
     }
 }
 
-#Preview {
-    SettingsView()
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
 }
+
